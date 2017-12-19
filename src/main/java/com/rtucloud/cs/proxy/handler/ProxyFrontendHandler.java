f80278ba19fbbf79de0d5e2c815a82592f57a265
liuyuan <405653510@qq.com>
2017-12-19 11:40:57 +0800

    解决一个BUG:防止出现发送不成功造成的永久不读取前端消息的错误.

--- src/main/java/com/rtucloud/cs/proxy/handler/ProxyFrontendHandler.java
+++ src/main/java/com/rtucloud/cs/proxy/handler/ProxyFrontendHandler.java
@@ -1,16 +1,7 @@
 package com.rtucloud.cs.proxy.handler;
 
-import java.net.SocketAddress;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-
 import com.rtucloud.cs.proxy.server.AppConfig;
 import com.rtucloud.cs.proxy.server.BackendPipeline;
-
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
@@ -25,141 +16,148 @@ import io.netty.channel.group.ChannelGroupFutureListener;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.concurrent.GlobalEventExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+
+import java.net.SocketAddress;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ProxyFrontendHandler extends SimpleChannelInboundHandler<byte[]> {
 
-	private static final Logger log = LoggerFactory.getLogger(ProxyFrontendHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(ProxyFrontendHandler.class);
 
-	// 代理服务器和目标服务器之间的通道（从代理服务器出去所以是outbound过境）
+    // 代理服务器和目标服务器之间的通道（从代理服务器出去所以是outbound过境）
+    public ApplicationContext applicationContext;
+    private volatile ChannelGroup allChannels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    private volatile Channel inboundChannel = null;
+    private AppConfig appConfig;
+    private volatile boolean frontendConnectStatus = false;
 
-	private volatile ChannelGroup allChannels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    public ProxyFrontendHandler(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+        this.appConfig = applicationContext.getBean(AppConfig.class);
+    }
 
-	private volatile Channel inboundChannel = null;
-	private AppConfig appConfig;
-	public ApplicationContext applicationContext;
+    /**
+     * Closes the specified channel after all queued write requests are flushed.
+     */
+    public static void closeOnFlush(Channel ch) {
+        if (ch.isActive()) {
+            ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+        }
+    }
 
-	private volatile boolean frontendConnectStatus = false;
+    /**
+     * 当客户端和代理服务器建立通道连接时，调用此方法
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        frontendConnectStatus = true;
 
-	public ProxyFrontendHandler(ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
-		this.appConfig = applicationContext.getBean(AppConfig.class);
-	}
+        SocketAddress clientAddress = ctx.channel().remoteAddress();
+        log.info("客户端地址：" + clientAddress);
 
-	/**
-	 * 当客户端和代理服务器建立通道连接时，调用此方法
-	 *
-	 * @param ctx
-	 * @throws Exception
-	 */
-	@Override
-	public void channelActive(ChannelHandlerContext ctx) throws Exception {
-		frontendConnectStatus = true;
+        List<String[]> remoteAddress = appConfig.getRemoteAddress();
 
-		SocketAddress clientAddress = ctx.channel().remoteAddress();
-		log.info("客户端地址：" + clientAddress);
+        /**
+         * 客户端和代理服务器的连接通道 入境的通道
+         */
 
-		List<String[]> remoteAddress = appConfig.getRemoteAddress();
+        inboundChannel = ctx.channel();
 
-		/**
-		 * 客户端和代理服务器的连接通道 入境的通道
-		 */
+        for (String[] str : remoteAddress) {
+            createBootstrap(inboundChannel, str[0].trim(), Integer.valueOf(str[1].trim()));
+        }
+    }
 
-		inboundChannel = ctx.channel();
+    /**
+     * 在这里接收客户端的消息 在客户端和代理服务器建立连接时，也获得了代理服务器和目标服务器的通道outbound，
+     * 通过outbound写入消息到目标服务器
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead0(final ChannelHandlerContext ctx, byte[] msg) throws Exception {
 
-		for (String[] str : remoteAddress) {
-			createBootstrap(inboundChannel, str[0].trim(), Integer.valueOf(str[1].trim()));
-		}
-	}
+        log.info("客户端消息");
+        allChannels.writeAndFlush(msg).addListener(new ChannelGroupFutureListener() {
 
-	/**
-	 * 在这里接收客户端的消息 在客户端和代理服务器建立连接时，也获得了代理服务器和目标服务器的通道outbound，
-	 * 通过outbound写入消息到目标服务器
-	 *
-	 * @param ctx
-	 * @param msg
-	 * @throws Exception
-	 */
-	@Override
-	public void channelRead0(final ChannelHandlerContext ctx, byte[] msg) throws Exception {
+            public void operationComplete(ChannelGroupFuture future) throws Exception {
 
-		log.info("客户端消息");
-		allChannels.writeAndFlush(msg).addListener(new ChannelGroupFutureListener() {
+                //防止出现发送不成功造成的永久不读取消息的错误.
+                ctx.channel().read();
 
-			public void operationComplete(ChannelGroupFuture future) throws Exception {
+//                if (future.isSuccess()) {
+//
+//                } else {
+//                    ctx.channel().close();
+//                }
+            }
+        });
 
-				if (future.isSuccess()) {
-					ctx.channel().read();
-				} else {
-					// future.channel().close();
-				}
-			}
-		});
+    }
 
-	}
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        log.info("代理服务器和客户端断开连接");
+        frontendConnectStatus = false;
+        allChannels.close();
+    }
 
-	@Override
-	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-		log.info("代理服务器和客户端断开连接");
-		frontendConnectStatus = false;
-		allChannels.close();
-	}
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.error("发生异常：", cause);
+        closeOnFlush(ctx.channel());
+    }
 
-	@Override
-	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-		log.error("发生异常：", cause);
-		closeOnFlush(ctx.channel());
-	}
+    public void createBootstrap(Channel inboundChannel, String host, int port) {
+        try {
+            Bootstrap bootstrap = new Bootstrap();
 
-	public void createBootstrap(Channel inboundChannel, String host, int port) {
-		try {
-			Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(inboundChannel.eventLoop());
+            bootstrap.channel(NioSocketChannel.class);
 
-			bootstrap.group(inboundChannel.eventLoop());
-			bootstrap.channel(NioSocketChannel.class);
+            bootstrap.handler(new BackendPipeline(inboundChannel, ProxyFrontendHandler.this, host, port));
 
-			bootstrap.handler(new BackendPipeline(inboundChannel, ProxyFrontendHandler.this, host, port));
+            ChannelFuture f = bootstrap.connect(host, port);
 
-			ChannelFuture f = bootstrap.connect(host, port);
+            f.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (future.isSuccess()) {
+                        allChannels.add(future.channel());
+                    } else {
+                        if (inboundChannel.isActive()) {
+                            System.out.println("Reconnect");
+                            final EventLoop loop = future.channel().eventLoop();
+                            loop.schedule(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ProxyFrontendHandler.this.createBootstrap(inboundChannel, host, port);
+                                }
+                            }, appConfig.getInterval(), TimeUnit.MILLISECONDS);
+                        } else {
+                            System.out.println("notActive");
+                        }
+                    }
+                    inboundChannel.read();
+                }
+            });
 
-			f.addListener(new ChannelFutureListener() {
-				@Override
-				public void operationComplete(ChannelFuture future) throws Exception {
-					if (future.isSuccess()) {
-						allChannels.add(future.channel());
-					} else {
-						if (inboundChannel.isActive()) {
-							System.out.println("Reconnect");
-							final EventLoop loop = future.channel().eventLoop();
-							loop.schedule(new Runnable() {
-								@Override
-								public void run() {
-									ProxyFrontendHandler.this.createBootstrap(inboundChannel, host, port);
-								}
-							}, appConfig.getInterval(), TimeUnit.MILLISECONDS);
-						} else {
-							System.out.println("notActive");
-						}
-					}
-					inboundChannel.read();
-				}
-			});
+        } catch (Exception e) {
 
-		} catch (Exception e) {
+        }
+    }
 
-		}
-	}
-
-	/**
-	 * Closes the specified channel after all queued write requests are flushed.
-	 */
-	public static void closeOnFlush(Channel ch) {
-		if (ch.isActive()) {
-			ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
-		}
-	}
-
-	public boolean isConnect() {
-		return frontendConnectStatus;
-	}
+    public boolean isConnect() {
+        return frontendConnectStatus;
+    }
 
 }
