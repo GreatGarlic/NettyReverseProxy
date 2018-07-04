5c6005692c523f1178ae87323789b105807a1016
liuyuan <405653510@qq.com>
2018-07-04 15:49:14 +0800

    提交

--- src/test/java/com/rtucloud/cs/proxy/BaseTest.java
+++ src/test/java/com/rtucloud/cs/proxy/BaseTest.java
@@ -1,4 +1,23 @@
 package com.rtucloud.cs.proxy;
 
+import org.junit.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class BaseTest {
+
+    @Test
+    public void test1() throws UnknownHostException {
+        InetAddress[] addresses=InetAddress.getAllByName("nandcloud.cn");
+        for(InetAddress addr:addresses)
+        {
+            System.out.println(addr);
+        }
+//        InetAddress   inetAddress=  InetAddress.getByName("");
+//
+//
+//        System.out.println(inetAddress.getHostAddress());
+
+    }
 }
