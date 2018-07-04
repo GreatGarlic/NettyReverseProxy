fa6c53747e37f8ace09a14a831f21e870f0a0b87
liuyuan <405653510@qq.com>
2018-07-04 22:48:01 +0800

    新建spring线程池

---
+++ src/main/java/com/rtucloud/cs/proxy/config/ExecutorConfig.java
@@ -0,0 +1,28 @@
+package com.rtucloud.cs.proxy.config;
+
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+
+@EnableAsync(mode = AdviceMode.ASPECTJ)
+@Configuration
+public class ExecutorConfig {
+
+    @Bean(name = "frontendWorkTaskExecutor")
+    public Executor newSingleThreadTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(1);
+        executor.setKeepAliveSeconds(0);
+        executor.setQueueCapacity(0);
+        executor.setDaemon(true);
+        executor.setThreadNamePrefix("frontendWorkTaskExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
