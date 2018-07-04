82a22d9662b35c815dd0f0d0f8e167ad78c926f0
liuyuan <405653510@qq.com>
2018-07-05 00:26:39 +0800

    提交

--- src/main/java/com/rtucloud/cs/proxy/server/Proxy.java
+++ src/main/java/com/rtucloud/cs/proxy/server/Proxy.java
@@ -1,20 +1,13 @@
 package com.rtucloud.cs.proxy.server;
 
 import com.rtucloud.cs.proxy.config.AppConfig;
-import com.rtucloud.cs.proxy.utils.ExecutorsUtils;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 @Component
 public class Proxy {
@@ -26,36 +19,42 @@ public class Proxy {
     public AppConfig appConfig;
     @Autowired
     FrontendPipeline frontendPipeline;
+    @Autowired
+    ExecutorsServer executorsServer;
 
     @PostConstruct
     public void initFrontend() {
-        ExecutorService executorService = ExecutorsUtils.newSingleThreadExecutor(FRONTEND_PROXY_THREAD_NAME);
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                LOGGER.debug("启动服务，端口:" + appConfig.getLocalPort());
 
-                EventLoopGroup bossGroup = new NioEventLoopGroup();
-                EventLoopGroup workerGroup = new NioEventLoopGroup();
-                try {
-                    ServerBootstrap b = new ServerBootstrap();
-                    b.group(bossGroup, workerGroup)
-                            .channel(NioServerSocketChannel.class)
-                            .childHandler(frontendPipeline)
-                            .childOption(ChannelOption.AUTO_READ, false);
+        Future future=   executorsServer.taskOne();
 
-                    ChannelFuture f = b.bind(Integer.valueOf(appConfig.getLocalPort())).sync();
 
-                    f.channel().closeFuture().sync();
-
-                } catch (Exception e) {
-                    LOGGER.error("代理服务器启动失败", e);
-                } finally {
-                    workerGroup.shutdownGracefully();
-                    bossGroup.shutdownGracefully();
-                }
-            }
-        });
+//        ExecutorService executorService = ExecutorsUtils.newSingleThreadExecutor(FRONTEND_PROXY_THREAD_NAME);
+//        executorService.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                LOGGER.debug("启动服务，端口:" + appConfig.getLocalPort());
+//
+//                EventLoopGroup bossGroup = new NioEventLoopGroup();
+//                EventLoopGroup workerGroup = new NioEventLoopGroup();
+//                try {
+//                    ServerBootstrap b = new ServerBootstrap();
+//                    b.group(bossGroup, workerGroup)
+//                            .channel(NioServerSocketChannel.class)
+//                            .childHandler(frontendPipeline)
+//                            .childOption(ChannelOption.AUTO_READ, false);
+//
+//                    ChannelFuture f = b.bind(Integer.valueOf(appConfig.getLocalPort())).sync();
+//
+//                    f.channel().closeFuture().sync();
+//
+//                } catch (Exception e) {
+//                    LOGGER.error("代理服务器启动失败", e);
+//                } finally {
+//                    workerGroup.shutdownGracefully();
+//                    bossGroup.shutdownGracefully();
+//                }
+//            }
+//        });
     }
 
     public void setFrontendPipeline(FrontendPipeline frontendPipeline) {
