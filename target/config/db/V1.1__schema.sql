122ca13e6e0924d963ab39122cda47d2ac03941e
liuyuan <405653510@qq.com>
2018-07-12 15:41:20 +0800

    提交

--- target/config/db/V1.1__schema.sql
+++ target/config/db/V1.1__schema.sql
@@ -1,2 +1,12 @@
-CREATE TABLE backend_server_info (id  integer, port integer not null, primary key (id));
-CREATE TABLE frontend_port_info (id  integer, port integer not null, primary key (id));
+CREATE TABLE frontend_port_info (
+  id   integer,
+  port integer not null,
+  primary key (id)
+);
+
+CREATE TABLE "backend_server_info" (
+  "id"   integer,
+  "ip"   TEXT(20) NOT NULL,
+  "port" integer  NOT NULL,
+  PRIMARY KEY ("id")
+);
