d3f5f4f85e2cf75e5d79a39926bb49f365eef45a
liuyuan <405653510@qq.com>
2018-11-08 15:37:55 +0800

    加入spring-boot-admin服务监控组件

--- src/main/java/com/rtucloud/cs/proxy/StartProgram.java
+++ src/main/java/com/rtucloud/cs/proxy/StartProgram.java
@@ -1,10 +1,12 @@
 package com.rtucloud.cs.proxy;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@EnableAdminServer
 public class StartProgram {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/rtucloud/cs/proxy/config/WebServerConfig.java
@@ -0,0 +1,85 @@
+package com.rtucloud.cs.proxy.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
+
+@Configuration
+public class WebServerConfig {
+
+    /**
+     * tomcat配置
+     */
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.getSession().setTimeout(Duration.ofMinutes(10));
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                AbstractHttp11Protocol httpProtocol = (AbstractHttp11Protocol) connector.getProtocolHandler();
+                httpProtocol.setCompression("on");
+                httpProtocol.setCompressibleMimeType("text/html,text/xml,text/plain,application/json,application/xml");
+            }
+        });
+        return factory;
+    }
+
+    /**
+     * 修改默认dispatcherServlet配置
+     */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(DispatcherServlet dispatcherServlet) {
+        ServletRegistrationBean registration = new ServletRegistrationBean<>(dispatcherServlet);
+        registration.getUrlMappings().clear();
+        registration.addUrlMappings("/");
+        registration.setAsyncSupported(true);
+        return registration;
+    }
+
+    /**
+     * SpringMVC配置
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            /**
+             * 跨域配置
+             */
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowCredentials(true)
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
+                        .maxAge(1800);
+            }
+
+            /**
+             * 资源加载配置
+             */
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+                registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources" +
+                        "/webjars/");
+            }
+
+        };
+    }
+
+
+}
