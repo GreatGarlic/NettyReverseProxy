45bbbc105e4c869d5e5bb61f20f96313e3568e8a
summit_cloud2018/liuyuan <Ly15891193398>
2019-05-13 17:42:55 +0800

    启动异步日志

--- src/main/resources/db/V1.2__data.sql
+++ src/main/resources/db/V1.2__data.sql
@@ -1,2 +1,2 @@
-insert into frontend_port_info (port) values (12001);
-insert into backend_server_info (ip,port) values ("192.168.141.222",12003);
+insert into frontend_port_info (port) values (9016);
+insert into backend_server_info (ip,port) values ("127.0.0.1",12003);
