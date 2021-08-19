d30509c69ac9d36df1021b238db2939b252c0dd1
liuyuan <405653510@qq.com>
2021-08-20 00:25:10 +0800

    add get internet IP method

--- src/test/java/com/rtucloud/cs/proxy/BaseTest.java
+++ src/test/java/com/rtucloud/cs/proxy/BaseTest.java
@@ -1,10 +1,16 @@
 package com.rtucloud.cs.proxy;
 
+import cn.hutool.socket.SocketUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
-
+@Slf4j
 public class BaseTest {
 
     @Test
@@ -19,5 +25,17 @@ public class BaseTest {
 //
 //        System.out.println(inetAddress.getHostAddress());
 
+    }
+    @Test
+    public void test4()throws Exception{
+        Socket socket= SocketUtil.connect("ns1.dnspod.net",6666);
+
+        InputStream is=socket.getInputStream();
+        BufferedReader br=new BufferedReader(new InputStreamReader(is));
+        String reply=br.readLine();
+        br.close();
+        is.close();
+        socket.close();
+        log.debug(reply);
     }
 }
